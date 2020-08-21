19ac417f78accdf8a5d995d0a551b33e7bd71b17
clbokea <clbo@kea.dk>
2020-08-21 18:05:08 +0200

    fix

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Basic String Unit Tests exercises
+Fill in the code in the methods of the StringExercises class so the tests in the StringExercisesTest class will pass.
+
+The test class is already created and can be found in test -> java -> kea.clbo -> StringExerciseTest
+
+If you run the tests now they all fail.
+
+Your job is to read the tests and afterwards create the right code for the methods in this StringExercises class.
--- src/test/java/kea/clbo/StringExercisesTest.java
+++ src/test/java/kea/clbo/StringExercisesTest.java
@@ -32,47 +32,17 @@ class StringExercisesTest {
 
     @Test
     void fixStart() {
-        /*
-
-              print()
-  print ('fix_start')
-  test(fix_start('babble'), 'ba**le')
-  test(fix_start('aardvark'), 'a*rdv*rk')
-  test(fix_start('google'), 'goo*le')
-  test(fix_start('donut'), 'donut')
-         */
-        
-
+        assertEquals("ba**le", exercises.fixStart("babble"));
+        assertEquals("a*rdv*rk", exercises.fixStart("aardvark"));
+        assertEquals("goo*le", exercises.fixStart("google"));
+        assertEquals("donut", exercises.fixStart("donut"));
     }
 
     @Test
     void mixUp() {
+        assertEquals("pox mid", exercises.mixUp("mix", "pod"));
+        assertEquals("dig donner", exercises.mixUp("dog", "dinner"));
+        assertEquals("spash gnort", exercises.mixUp("gnash", "sport"));
+        assertEquals("fizzy perm", exercises.mixUp("pezzy", "firm"));
     }
-
-        /*
-
-  print()
-  print ('both_ends')
-  test(both_ends('spring'), 'spng')
-  test(both_ends('Hello'), 'Helo')
-  test(both_ends('a'), '')
-  test(both_ends('xyz'), 'xyyz')
-
-
-
-
-  print()
-  print ('mix_up')
-  test(mix_up('mix', 'pod'), 'pox mid')
-  test(mix_up('dog', 'dinner'), 'dig donner')
-  test(mix_up('gnash', 'sport'), 'spash gnort')
-  test(mix_up('pezzy', 'firm'), 'fizzy perm')
-
-main()
-
-
-
-    */
-
-
 }
