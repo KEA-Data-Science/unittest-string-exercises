2ee241039336b7f99aedefea4265bf64fe6e5c44
kiancn <kian.tema@gmail.com>
2020-09-17 14:39:14 +0200

    Update StringExercises.java

--- src/main/java/kea/clbo/StringExercises.java
+++ src/main/java/kea/clbo/StringExercises.java
@@ -8,7 +8,8 @@ package kea.clbo;
 //    Your job is to read the tests and aftervards create the right code for the methods in this StringExercises class.
 
 
-public class StringExercises {
+public class StringExercises
+{
 
 //    A. donuts
 //    Given an int count of a number of donuts, return a string of the form 'Number of donuts: <count>', where <count> is the number passed in.
@@ -17,8 +18,11 @@ public class StringExercises {
 //    So donuts(5) returns 'Number of donuts: 5'
 //    and donuts(23) returns 'Number of donuts: many'
 
-    public String donuts(int count) {
-        return null;
+    public String donuts(int count)
+    {
+        String result = "Number of donuts: ";
+        result += count < 10 ? count : "many";
+        return result;
     }
 
 //    B. both_ends
@@ -27,8 +31,16 @@ public class StringExercises {
 //    so 'spring' yields 'spng'. However, if the string length
 //    is less than 2, return instead the empty string.
 
-    public String bothEnds(String s) {
-        return null;
+    public String bothEnds(String s)
+    {
+        String result = "";
+
+        if(s.length() < 2){return result;}
+
+        result += s.substring(0, 2);
+        result += s.substring(s.length() - 2, s.length());
+
+        return result;
     }
 
 //     C. fix_start
@@ -38,10 +50,17 @@ public class StringExercises {
 //     the first char itself.
 //     e.g. 'babble' yields 'ba**le'
 
-    public String fixStart(String s) {
-        return null;
-    }
+    public String fixStart(String s)
+    {
+        String result = "";
 
+        char firstChar = s.charAt(0);
+
+        result = s.replaceAll("" + String.valueOf(firstChar) + "", "*");
+        result = result.replaceFirst("\\*", String.valueOf(firstChar));
+
+        return result;
+    }
 
 
 //    D. MixUp
@@ -52,8 +71,16 @@ public class StringExercises {
 //      'dog', 'dinner' -> 'dig donner'
 //    Assume a and b are length 2 or more.
 
-    public String mixUp(String a, String b) {
-        return null;
+    public String mixUp(String a, String b)
+    {
+        String startA = a.substring(0, 2);
+        String startB = b.substring(0, 2);
+
+        String resultA = startB + a.substring(2);
+        String resultB = startA + b.substring(2);
+
+        String result = resultA + " " + resultB;
+        return result;
     }
 
 }
